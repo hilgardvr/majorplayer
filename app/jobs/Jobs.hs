7d1c7d166cf3d999157c98a3dee9d4e85da7299c
hilgard <hilgardvr@gmail.com>
2024-05-17 07:14:58 +0200

    Feat: Add leaderboard refresh 5-7

--- app/jobs/Jobs.hs
+++ app/jobs/Jobs.hs
@@ -10,19 +10,18 @@ import Control.Concurrent (ThreadId)
 
 rankingRefreshSchedule :: Text
 rankingRefreshSchedule = "0 12 * * 1-3"
---rankingRefreshSchedule = "* * * * 1-3"
 
 leaderboardRefreshSchedule :: Text
 leaderboardRefreshSchedule = "0 16-23/4 * * 4-7"
 
-
 leaderboardRefreshSchedule2 :: Text
 leaderboardRefreshSchedule2 = "0 0-12/4 * * 1"
---leaderboardRefreshSchedule = "* * * * 1-3"
+
+leaderboardRefreshSchedule3 :: Text
+leaderboardRefreshSchedule3 = "0 0-5/4 * * 5-7"
 
 fixtureRefreshSchedule :: Text
 fixtureRefreshSchedule = "0 18 * * 1"
---fixtureRefreshSchedule = "* * * * 1-3"
 
 executeScheduleJobs :: DataClientApi a => Env -> a -> IO [ThreadId]
 executeScheduleJobs env client = do
@@ -30,6 +29,7 @@ executeScheduleJobs env client = do
         addJob (refreshRankings client) rankingRefreshSchedule
         addJob (refreshLeaderboard client) leaderboardRefreshSchedule
         addJob (refreshLeaderboard client) leaderboardRefreshSchedule2
+        addJob (refreshLeaderboard client) leaderboardRefreshSchedule3
         addJob (refreshFixtures client) fixtureRefreshSchedule
     logger env DEBUG $ "starting crons: " ++ show tids
     return tids
